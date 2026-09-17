4c52a5653306d54a003ef57ddaef8e5e5dab7301
Sanjaya <sanjaya.w@jinasena.com.lk>
2026-09-17 09:50:19 +0530

    Port 13 Studio field selection options from CDB

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Jinasena : Module : Accounting',
-    'version': '17.0.0.0.101',
+    'version': '17.0.0.0.102',
     'summary': 'Studio-to-Python port for BugFix-Accounting',
     'author': 'Jinasena Agricultural Machinery (Pvt) Ltd.',
     'category': 'Accounting',
@@ -317,6 +317,7 @@
         'data/menus_f6.xml',
         'data/menus_visual_fixes.xml',
         'views/qweb_studio_ported.xml',
+        'data/field_selections_ported.xml',
     ],
     'installable': True,
     'auto_install': False,
