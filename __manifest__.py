f12ad7817933fc744125a90ce5072f2659f66e5b
SanjayaWeerasinghe <sanjayarajans26407@gmail.com>
2026-09-23 23:49:26 +0530

    v0.0.112: Accounting menu cleanup - 6 own-module duplicates deactivated
    Kill 5 menu_f6_* Studio-routing duplicates + 1 menu_1191_credit_note:
    - menu_f6_pump_product_costing (dup of menu_1085_)
    - menu_f6_pump_costing_worksheet (dup of menu_1173_)
    - menu_f6_advance_payment_account (dup of menu_1323_)
    - menu_f6_invoice_wise_revenue_report (dup of menu_1690_)
    - menu_f6_invoice_wise_revenue_report_1 (extra dup, same target)
    - menu_1191_credit_note (dup of core account.menu_action_move_out_refund_type)
    
    Companion cleanups in BugFix-Purchase v0.1.0.164 (1 dupe),
    BugFix-Studio-Misc v0.0.101 (3 dupes),
    BugFix-Analytics v0.1.0.13 (1 dupe).

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Jinasena : Module : Accounting',
-    'version': '17.0.0.0.111',
+    'version': '17.0.0.0.112',
     'summary': 'Studio-to-Python port for BugFix-Accounting',
     'author': 'Jinasena Agricultural Machinery (Pvt) Ltd.',
     'category': 'Accounting',
